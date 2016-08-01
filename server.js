3a75abf8ac0131f2e578c83e36706f2529b8be11
Mike Baldwin <mdb255.dev@gmail.com>
2016-07-31 21:21:41 -0400

    Initial test commit

---
+++ server.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Mike on 7/31/2016.
+ */
+
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = process.env.PORT || 8080;
+
+// App
+const app = express();
+app.get('/', function (req, res) {
+  res.send('Hello world\n');
+});
+
+app.listen(PORT);
+console.log('Running on http://localhost:' + PORT);
